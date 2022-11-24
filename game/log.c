f28bf24a1772e4236204b683da2949ec440206d4
Nuno Das Neves <ndasneves@gmail.com>
2022-11-23 22:56:53 -0800

    add pretty printed timestamp to logs

--- game/log.c
+++ game/log.c
@@ -2,24 +2,93 @@
 #include<stdio.h>
 #include<stdarg.h>
 #include<ctype.h>
-
+#include<SDL.h>
 #include"platform.h"
 #include"log.h"
 
 static void *log_buf;
 #define LOG_BUF_SZ PAGE_SIZE
 
-static char *log_level_strings[] = {
-    "INFO",
-    "DEBUG",
-    "WARN",
-    "ERROR"
-};
+// timestamp should hold "XXXXXXXXXXXXdXXhXXmXX.XXXs"; enough for u64 ms
+#define LOG_TIMESTAMP_SZ 30 // few extra just in case
 
-static int format_time(char *buf, u64 time_ms)
+static size_t u64toa(u64 num, char *buf)
 {
-    // TODO
-    return 0;
+    ASSERT(buf);
+
+    if (num == 0) {
+        *buf = '0';
+        return 1;
+    }
+
+    char *start = buf;
+    size_t len = 0;
+    size_t base = 10;
+
+    while (num) {
+        u64 div = num / base;
+        u64 rem = num - div * base;
+        ASSERT(rem < base);
+        *buf++ = '0' + (char)rem;
+        num = div;
+        len++;
+    }
+    buf--;
+    while (start < buf) {
+        char tmp = *buf;
+        *buf-- = *start;
+        *start++ = tmp;
+    }
+
+    return len;
+}
+
+static void append_time_field(u64 time, char suffix, char **buf, size_t *remaining, size_t *len)
+{
+    size_t n = u64toa(time, *buf);
+    ASSERT(*remaining > n + 1);
+    *remaining -= n + 1;
+    *buf += n;
+    **buf = suffix;
+    *buf += 1; // note *buf++ would increment the char ** instead of the char *
+    *len += n + 1;
+}
+
+static size_t format_time_ms(u64 ms, char* buf, size_t size)
+{
+    u64 secs = ms / 1000;
+    u64 mins = secs / 60;
+    u64 hours = mins / 60;
+    u64 days = hours / 24;
+    ms -= secs * 1000;
+    secs -= mins * 60;
+    mins -= hours * 60;
+    hours -= days * 24;
+    u64 fields[3] = {days, hours, mins};
+    static const char names[3] = {'d', 'h', 'm'};
+
+    size_t len = 0;
+    size_t remaining = size;
+
+    // d,h,m work the same
+    for (int i = 0; i < 3; ++i) {
+        if (fields[i] == 0) {
+            continue;
+        }
+        append_time_field(fields[i], names[i], &buf, &remaining, &len);
+    }
+
+    // if secs is 0 we still want "0." before ms
+    append_time_field(secs, '.', &buf, &remaining, &len);
+
+    // if ms is 0 we still want "0s"
+    append_time_field(ms, 's', &buf, &remaining, &len);
+    ASSERT(remaining > 0);
+
+    // in case we need it as a string
+    *buf = '\0';
+
+    return len;
 }
 
 static void _log_print(const char *str, size_t len)
@@ -56,34 +125,42 @@ void log_timestamp(const char *fmt, ...)
     ASSERT(fmt);
     ASSERT(log_buf);
 
+    size_t len = 0; // length of final string, including appended \n
     char* buf = (char *)log_buf;
     bool truncated = false;
 
-    // TODO timestamp
+    size_t remaining = LOG_BUF_SZ;
+    *buf++ = '[';
+    size_t time_len = format_time_ms(SDL_GetTicks64(), buf, LOG_TIMESTAMP_SZ);
+    ASSERT(time_len < LOG_BUF_SZ - 3);
+    buf += time_len;
+    *buf++ = ']';
+    *buf++ = ' ';
+    remaining -= time_len + 3;
+    len += time_len + 3;
 
     va_list args;
     va_start(args, fmt);
-    int result = vsnprintf(buf, LOG_BUF_SZ, fmt, args);
+    int result = vsnprintf(buf, remaining, fmt, args);
     va_end(args);
 
     if (result > 0) {
         char *end;
         char *start;
-        size_t len; // length of final string, including appended \n
 
         // Enforce single-line printing by removing \n's
         /*
-         * if result >= LOG_BUF_SZ, vsnprintf truncated the string
+         * if result >= remaining, vsnprintf truncated the string
          * but we want to add a \n on the end, so treat buffer as 1 char smaller
          */
-        if (result >= LOG_BUF_SZ - 1) {
+        if (result >= remaining - 1) {
             truncated = true;
-            len = LOG_BUF_SZ - 1;
+            len += remaining - 1;
             // overwrite the last char before the null
-            end = &buf[LOG_BUF_SZ - 2];
+            end = &buf[remaining - 2];
         } else {
             // extend string by 1 char for \n
-            len = result + 1;
+            len += result + 1;
             // append another null char
             buf[len] = '\0';
             // replace first null char
@@ -97,7 +174,7 @@ void log_timestamp(const char *fmt, ...)
             }
         }
 
-        _log_print(buf, len);
+        _log_print(log_buf, len);
 
     } else {
         log_warn("%s: formatting failed", __func__);
@@ -113,6 +190,7 @@ bool log_init()
     if (!log_buf) {
         return false;
     }
+    // TODO
     // raw because SDL isn't initted yet
     log_raw("Log init success\n");
 
